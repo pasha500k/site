7e8f7344bd0912952d56d093c4f5f4c6ce31cafc
pasha500k <ps.stepa77@gmail.com>
2025-12-16 23:25:51 +0300

    Send /code response only to requesting admin

--- app.py
+++ app.py
@@ -263,8 +263,9 @@ async def cmd_code(msg: types.Message):
         await msg.answer("Бот выключен: не настроены TELEGRAM_BOT_TOKEN / TELEGRAM_ADMIN_ID")
         return
     if msg.from_user.id in TELEGRAM_ADMIN_IDS:
-        await msg.answer("Отправляю актуальный код всем администраторам.")
-        await send_tg_async(current_date_str, current_daily_code, TELEGRAM_ADMIN_IDS)
+        target_admin = [msg.from_user.id]
+        await msg.answer("Отправляю актуальный код вам в личные сообщения.")
+        await send_tg_async(current_date_str, current_daily_code, target_admin)
     else:
         await msg.answer("Недостаточно прав.")
 
